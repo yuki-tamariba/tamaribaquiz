f308b3b4115951fbdbf7eb2a656a78661416b668
yuki-tamariba <yuki.useself@gmail.com>
2021-06-12 17:17:43 +0900

    [add]StartQuizService

---
+++ Services/StartQuizService.cs
@@ -0,0 +1,67 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.WebUtilities;
+using Play.Tamariba.QuizGame.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Play.Tamariba.QuizGame.Service
+{
+public class StartQuiz
+{
+  static readonly string ChannelAccessToken = "rnxbLwzfOzXTlgoy6hDCtNnqOYxT3UjEAqlPzb87pTCPdgHjGjoBrZvZGvpqq0JbqTVC79/H0hnkCU86vcy/85ihoHmgn6GbmJdv+e0OlvX3rlVpEeOuazrs6cclvawxRUA0kdAzu7xufmpku2EIFgdB04t89/1O/w1cDnyilFU="; 
+  static readonly string LineEndpoint = "https://api.line.me/v2/bot/message/reply";
+  public readonly static HttpClient client;
+  static StartQuiz()
+  {
+      client = new HttpClient();
+      client.DefaultRequestHeaders.Add("authorization", $@"Bearer {ChannelAccessToken}");
+  }
+  [FunctionName("StartQuiz")]
+  public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ExecutionContext context, ILogger log) {
+    using var stream = new System.IO.StreamReader(req.Body);
+    {
+      
+      log.LogInformation("post");
+      using var jsonReaer = new JsonTextReader(stream);
+      dynamic json = JsonSerializer.CreateDefault().Deserialize(jsonReaer);
+      JArray events = json.events;
+      foreach (dynamic item in events)
+      {
+        string reply_token = item.replyToken;
+        string reply_text = item.message.text;
+        try	
+        {
+          var payload = new{
+            replyToken = reply_token,
+            messages =  new[]{
+              new{
+                type = "text",
+                text =  reply_text
+              }
+            }
+          };
+
+          HttpResponseMessage response = await client.PostAsJsonAsync(LineEndpoint, payload);
+          response.EnsureSuccessStatusCode();
+          string responseBody = await response.Content.ReadAsStringAsync();
+
+          log.LogInformation(responseBody);
+        }
+        catch(HttpRequestException e)
+        {
+          log.LogInformation("\nException Caught!");	
+          log.LogInformation("Message :{0} ",e.Message);
+        }
+      }
+    }
+    return new OkObjectResult(new{});
+  }} 
+}
