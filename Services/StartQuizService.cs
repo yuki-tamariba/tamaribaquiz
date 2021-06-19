05dc538120ca455cbf10e984d050694ddc334345
yuki-tamariba <yuki.useself@gmail.com>
2021-06-19 20:54:10 +0900

    [add]20210629関数組み合わせて問題を表示させたい

--- Services/StartQuizService.cs
+++ Services/StartQuizService.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json.Linq;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace Play.Tamariba.QuizGame.Service
 {
@@ -33,32 +34,113 @@ public class StartQuiz
       using var jsonReaer = new JsonTextReader(stream);
       dynamic json = JsonSerializer.CreateDefault().Deserialize(jsonReaer);
       JArray events = json.events;
+      //LoadQuizService
+      QuizService service = new QuizService(context);
+      LoadQuizService QuizService_test = new LoadQuizService(service);
       foreach (dynamic item in events)
       {
         string reply_token = item.replyToken;
         string reply_text = item.message.text;
+        
+        Task<object[]> reply_quiz =  QuizService_test.LoadQuiz();
+
+        //後で消す部分
+        var quiz_text = await service.LoadQuiz();
+        var selections = quiz_text.Selections.Select(kv => new
+            {
+                type = "text",
+                size = "lg",
+                text = @$"{kv.Key} {kv.Value}"
+            });
         //
-        if(reply_text == "クイズ")
-        {
-          reply_text = "じゅんびちゅー";
-        }
         try	
         {
-          var payload = new{
-            replyToken = reply_token,
-            messages =  new[]{
-              new{
-                type = "text",
-                text =  reply_text
+          if(reply_text == "クイズ"){
+            var payload = new{
+              replyToken = reply_token,
+              messages =  new object[]{
+                new {
+                    type = "flex",
+                    altText = "問題",
+                    contents = new {
+                        type = "bubble",
+                        body = new {
+                            type = "box",
+                            layout = "vertical",
+                            contents = new object[] {
+                                new {
+                                    type = "text",
+                                    text = "問題",
+                                    weight = "bold",
+                                    size = "xxl"
+                                },
+                                
+                                new {
+                                    type = "text",
+                                    text = "quiz.Question",
+                                    wrap = true,
+                                    size = "lg",
+                                    margin = "lg",
+                                    flex = 5
+                                },
+                                new {
+                                    type = "box",
+                                    layout = "vertical",
+                                    position = "relative",
+                                    spacing = "sm",
+                                    margin = "lg",
+                                    contents = selections.ToArray()
+                                }
+                            }
+                        },
+                        
+                        footer = new {
+                            type = "box",
+                            layout = "vertical",
+                            contents = new object[] {
+                                new {
+                                    type = "button",
+                                    action = new {
+                                        type = "postback",
+                                        label = "答えを見る",
+                                        data = $@"questionId={1}"
+                                    },
+                                    gravity = "center",
+                                    style = "primary",
+                                    height = "md"
+                                }
+                              
+                            }
+                        }
+                        
+                    }
+                }
               }
-            }
-          };
+            };
 
-          HttpResponseMessage response = await client.PostAsJsonAsync(LineEndpoint, payload);
-          response.EnsureSuccessStatusCode();
-          string responseBody = await response.Content.ReadAsStringAsync();
+            HttpResponseMessage response = await client.PostAsJsonAsync(LineEndpoint, payload);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
 
-          log.LogInformation(responseBody);
+            log.LogInformation(responseBody);
+          }
+          else{
+            var payload = new{
+              replyToken = reply_token,
+              messages =  new[]{
+                new{
+                  type = "text",
+                  text =  reply_text
+                }
+              }
+            };
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(LineEndpoint, payload);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            log.LogInformation(responseBody);
+          }
         }
         catch(HttpRequestException e)
         {
@@ -68,5 +150,6 @@ public class StartQuiz
       }
     }
     return new OkObjectResult(new{});
-  }} 
+  }
+  } 
 }
